f5ac5ead31b02f6ac10817fbeb97041f8bdb61ce
Biplav Choudhury <biplav93@gmail.com>
2020-02-07 19:23:37 -0500

    formatting

--- circles.py
+++ circles.py
@@ -18,8 +18,7 @@ import traci
 
 def get_options():
     opt_parser = optparse.OptionParser()
-    opt_parser.add_option("--nogui", action="store_true",
-                         default=False, help="run the commandline version of sumo")
+    opt_parser.add_option("--nogui", action="store_true", default=False, help="run the commandline version of sumo")
     options, args = opt_parser.parse_args()
     return options
 
@@ -27,23 +26,20 @@ def get_options():
 # contains TraCI control loop
 def run():
     step = 0
+    print("vehID time(*0.01s) x_speed y_speed x_pos y_pos", file = open("locations.txt", "a"))
     while traci.simulation.getMinExpectedNumber() > 0:
-        traci.simulationStep(step+0.1)
-        # print(step)
+        traci.simulationStep(step+.01) # modify time step. time is in seconds
 
 # IMPTT: the variable step has no relation to time inside the SUMO simulation, it is just a variable that we we use to keep track of time,
 # and the command traci.simulationStep(step+0.1) means running the simulation at time = step + 0.1
 
-        # det_vehs = traci.inductionloop.getLastStepVehicleIDs("det_0")
-        # for veh in det_vehs:
-        #     print(veh)
-        #     traci.vehicle.changeLane(veh, 2, 25)
         if step > 173: # by 173 seconds, all the vehicles enter the simulation
             for veh in traci.vehicle.getIDList():
-                print(veh, step, traci.vehicle.getLateralSpeed(veh), traci.vehicle.getSpeed(veh), traci.vehicle.getPosition(veh), file = open("locations.txt", "a"))
+                veh1 = str(veh).split(".")[1]
+                print(veh1, int(step*100), traci.vehicle.getSpeed(veh), traci.vehicle.getLateralSpeed(veh), traci.vehicle.getPosition(veh)[0], traci.vehicle.getPosition(veh)[1], file = open("locations.txt", "a"))
         # print(traci.vehicle.getIDList())
 
-        step += 0.1
+        step += 0.01 # modify time step
 
     traci.close()
     sys.stdout.flush()
